85c80ec1f0f4841f2f1b136c4e7090015e2c052b
Nicolas Lopez <nicolasrrlopez@gmail.com>
2022-09-24 20:30:27 -0300

    1er Entrega Nicolas Lopez

---
+++ src/Components/CartWidGet/CartWidGet.js
@@ -0,0 +1,14 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+
+const CartWidGet = () => {
+    return (
+        
+        <FontAwesomeIcon icon={faCartShopping} />
+        
+    )
+}
+
+export default CartWidGet;
